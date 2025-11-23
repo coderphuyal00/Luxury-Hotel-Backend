0bc88e35ff47461792ad2c9b183087270a4a3660
coderphuyal00 <raj.venomousraghav415@gmail.com>
2025-11-23 15:17:26 +0545

    Add authentication level

--- Booking/views.py
+++ Booking/views.py
@@ -4,32 +4,36 @@ from django.conf import settings
 # Create your views here.
 from .models import Booking
 from Room.models import Room
+from django.contrib.auth import get_user_model
 # full_message = f"Question from {name} <{email}>:\n\n{question} for {department} deparment."
 # send_mail(subject, full_message, settings.DEFAULT_FROM_EMAIL, [settings.ADMIN_EMAIL])
 
 def book_room(request, id):
-    room = get_object_or_404(Room, id=id)
+    if request.user.is_authenticated:
+        room = get_object_or_404(Room, id=id)
 
-    if room.status == "booked":
-        return render(request, 'already_booked.html', {"room": room})
+        if room.status == "booked":
+            return render(request, 'already_booked.html', {"room": room})
 
-    if request.method == 'POST':
-        name = request.POST.get('customer_name')
-        phone = request.POST.get('phone_number')
-        check_in = request.POST.get('check_in')
-        check_out = request.POST.get('check_out')
+        if request.method == 'POST':
+            name = request.POST.get('customer_name')
+            phone = request.POST.get('phone_number')
+            check_in = request.POST.get('check_in')
+            check_out = request.POST.get('check_out')
 
-        Booking.objects.create(
-            room=room,
-            customer_name=name,
-            phone_number=phone,
-            check_in=check_in,
-            check_out=check_out
-        )
+            Booking.objects.create(
+                room=room,
+                customer_name=name,
+                phone_number=phone,
+                check_in=check_in,
+                check_out=check_out
+            )
 
-        room.status = 'booked'
-        room.save()
+            room.status = 'booked'
+            room.save()
 
-        return redirect('room_list')
+            return redirect('room_list')
+    else:
+        return redirect('login')
 
     return render(request, 'book_room.html', {'room': room})
